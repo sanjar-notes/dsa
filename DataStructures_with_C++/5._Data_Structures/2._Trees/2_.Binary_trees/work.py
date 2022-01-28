823c90a6daf615c1d8e5ab1fca91a461a073d21d
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2022-01-28 19:05:04 +0530

    Remove Zim files

--- DataStructures_with_C++/5._Data_Structures/2._Trees/2_.Binary_trees/work.py
+++
@@ -1,6 +0,0 @@
-import os
-folders = list(filter(lambda i: i.endswith('.txt') and not os.path.isdir(i), os.listdir()))
-count = 0
-for f in folders:
-    newname = str(count:=count+1)+f[1:]
-    os.rename(f, newname)
